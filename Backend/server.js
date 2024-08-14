c2d853e9f5ccc70cf5597a55c4f62c29c8c84422
jflatorreo <jflatorreo@unal.edu.co>
2024-08-14 14:19:21 -0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -22,3 +23,7 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/build/index.html'));
+});
